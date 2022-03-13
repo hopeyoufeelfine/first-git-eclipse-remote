a06c406686798cfa49c1654476c1e9b94cfa3cfa
hopeyoufeelfine <hopeyoufeelhappy@hotmail.com>
2022-03-14 00:28:56 +0800

    创建分支

--- first-maven/src/main/java/org/itguigu/maven/firstmavenweb/FirstMaven.java
+++ first-maven/src/main/java/org/itguigu/maven/firstmavenweb/FirstMaven.java
@@ -2,7 +2,7 @@ package org.itguigu.maven.firstmavenweb;
 
 public class FirstMaven {
     public String sayHello(String name) {
-        return "Hello World," + name + "!";
+        return "Hello World, 创建分支" + name + "!";
     }
 }
 
