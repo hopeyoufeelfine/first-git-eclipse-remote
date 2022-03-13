68b14dca081c0db33062db815d07deb11a97991e
hopeyoufeelfine <hopeyoufeelhappy@hotmail.com>
2022-03-13 22:28:12 +0800

    springboot-admin-server first commit

---
+++ springboot-admin-server/src/test/java/org/itheima/springboot/springbootadminserver/SpringbootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itheima.springboot.springbootadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
